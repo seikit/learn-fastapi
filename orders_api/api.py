42b14c58b51e8d28cbd59dc8be3fbdec4b67bf6a
eder <seikitsi@gmail.com>
2021-11-06 19:43:07 -0400

    feat: enhance documentation

--- orders_api/api.py
+++ orders_api/api.py
@@ -6,6 +6,8 @@ from fastapi import FastAPI, Depends, Request
 from fastapi.responses import JSONResponse
 
 from exceptions.custom_exceptions import OrderNotFoundError, CommunicationFailedError
+from schemas.error import ErrorResponse
+from schemas.health import HealthCheck
 from schemas.item import Item
 
 app = FastAPI()
@@ -21,15 +23,19 @@ def handle_communication_error(request: Request, exc: CommunicationFailedError):
     return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Service communication failed"})
 
 
-@app.get('/health')
+@app.get('/health', tags=['health'], summary='System health', description='Check if the system is running',
+         response_model=HealthCheck)
 async def health_check():
-    return {'status': 'ok'}
+    return HealthCheck(status='ok')
 
 
 def get_order_items(uuid: UUID) -> List[Item]:
     pass
 
 
-@app.get('/orders/{uuid}/items')
+@app.get('/orders/{uuid}/items', tags=['orders'], summary='Order items', description='Get the item of an order',
+         response_model=List[Item], responses={
+        HTTPStatus.NOT_FOUND.value: {'description': 'Order not found', 'model': ErrorResponse},
+        HTTPStatus.BAD_GATEWAY.value: {'description': 'Service communication failed', 'model': ErrorResponse}})
 def read_items(items: List[Item] = Depends(get_order_items)):
     return items
