1f2abca0a9e9c9dc5f7c7b1a9dd42f205a521ea5
eder <seikitsi@gmail.com>
2021-11-06 21:15:15 -0400

    fix: black reformat following PEP8

--- orders_api/api.py
+++ orders_api/api.py
@@ -18,27 +18,53 @@ app = FastAPI()
 
 @app.exception_handler(OrderNotFoundError)
 def handle_order_not_found_error(request: Request, exc: OrderNotFoundError):
-    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Order not found"})
+    return JSONResponse(
+        status_code=HTTPStatus.NOT_FOUND,
+        content={"message": "Order not found"},
+    )
 
 
 @app.exception_handler(CommunicationFailedError)
-def handle_communication_error(request: Request, exc: CommunicationFailedError):
-    return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Service communication failed"})
+def handle_communication_error(
+    request: Request, exc: CommunicationFailedError
+):
+    return JSONResponse(
+        status_code=HTTPStatus.BAD_GATEWAY,
+        content={"message": "Service communication failed"},
+    )
 
 
-@app.get('/health', tags=['health'], summary='System health', description='Check if the system is running',
-         response_model=HealthCheck)
+@app.get(
+    "/health",
+    tags=["health"],
+    summary="System health",
+    description="Check if the system is running",
+    response_model=HealthCheck,
+)
 async def health_check():
-    return HealthCheck(status='ok')
+    return HealthCheck(status="ok")
 
 
 def get_order_items(uuid: UUID) -> List[Item]:
     pass
 
 
-@app.get('/orders/{uuid}/items', tags=['orders'], summary='Order items', description='Get the item of an order',
-         response_model=List[Item], responses={
-        HTTPStatus.NOT_FOUND.value: {'description': 'Order not found', 'model': ErrorResponse},
-        HTTPStatus.BAD_GATEWAY.value: {'description': 'Service communication failed', 'model': ErrorResponse}})
+@app.get(
+    "/orders/{uuid}/items",
+    tags=["orders"],
+    summary="Order items",
+    description="Get the item of an order",
+    response_model=List[Item],
+    responses={
+        HTTPStatus.NOT_FOUND.value: {
+            "description": "Order not found",
+            "model": ErrorResponse,
+        },
+        HTTPStatus.BAD_GATEWAY.value: {
+            "description": "Service communication failed",
+            "model": ErrorResponse,
+        },
+    },
+)
 def read_items(items: List[Item] = Depends(get_order_items)):
     return items
